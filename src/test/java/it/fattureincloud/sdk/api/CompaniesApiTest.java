a7f37c6d8e36fd44f9c51d5c4eca00ef25a6b235
Emanuele Saccomandi <emanuelesaccomandi@fattureincloud.it>
2021-12-28 14:12:04 +0100

    test: added CompaniesApi tests

--- src/test/java/it/fattureincloud/sdk/api/CompaniesApiTest.java
+++ src/test/java/it/fattureincloud/sdk/api/CompaniesApiTest.java
@@ -13,15 +13,17 @@
 
 package it.fattureincloud.sdk.api;
 
+import it.fattureincloud.sdk.ApiClient;
 import it.fattureincloud.sdk.ApiException;
-import it.fattureincloud.sdk.model.GetCompanyInfoResponse;
+import it.fattureincloud.sdk.model.*;
+import okhttp3.*;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * API tests for CompaniesApi
@@ -29,22 +31,100 @@ import java.util.Map;
 @Disabled
 public class CompaniesApiTest {
 
-    private final CompaniesApi api = new CompaniesApi();
+    private CompaniesApi mockApi(final String serializedBody, final Call remoteCall) throws IOException {
+        final OkHttpClient okHttpClient = Mockito.mock(OkHttpClient.class);
 
-    
+        Response.Builder builder = new Response.Builder()
+                .request(new Request.Builder().url("https://api-v2.fattureincloud.it").build())
+                .protocol(Protocol.HTTP_1_1)
+                .code(200)
+                .message("");
+        if (serializedBody != null) {
+            builder = builder.body(
+                    ResponseBody.create(
+                            serializedBody,
+                            MediaType.parse("application/json")
+                    ));
+        }
+
+        final Response response = builder.build();
+
+        Mockito.when(remoteCall.execute()).thenReturn(response);
+        Mockito.when(okHttpClient.newCall(Mockito.any())).thenReturn(remoteCall);
+
+        ApiClient client = new ApiClient(okHttpClient);
+
+        return new CompaniesApi(client);
+    }
+
+
     /**
      * Get Company Info
-     *
+     * <p>
      * Gets the company detailed info.
      *
-     * @throws ApiException
-     *          if the Api call fails
+     * @throws ApiException if the Api call fails
      */
     @Test
-    public void getCompanyInfoTest() throws ApiException {
-        Integer companyId = null;
-                GetCompanyInfoResponse response = api.getCompanyInfo(companyId);
-        // TODO: test validations
+    public void getCompanyInfoTest() throws ApiException, IOException {
+        String result = "{\"data\":{\"id\":12346,\"name\":\"Studio Commercialista\",\"email\":\"mario.rossi@example.com\",\"type\":\"accountant\",\"access_info\":{\"role\":\"master\",\"through_accountant\":false},\"plan_info\":{\"limits\":{\"clients\":5000,\"suppliers\":5000,\"products\":5000,\"documents\":3000},\"functions\":{\"archive\":true,\"cerved\":true,\"document_attachments\":true,\"e_invoice\":true,\"genius\":true,\"mail_tracking\":true,\"payment_notifications\":true,\"paypal\":true,\"receipts\":true,\"recurring\":true,\"smtp\":true,\"sofort\":false,\"stock\":true,\"subaccounts\":true,\"tessera_sanitaria\":true,\"ts_digital\":true,\"ts_invoice_trading\":true,\"ts_pay\":true},\"functions_status\":{\"ts_digital\":{\"active\":true},\"ts_pay\":{\"active\":false}}},\"accountant_id\":12345,\"is_accountant\":true}}";
+
+        Call mockCall = Mockito.mock(Call.class);
+        CompaniesApi api = mockApi(result, mockCall);
+
+        Integer companyId = 11111;
+
+        CompanyInfo expected = new CompanyInfo()
+                .id(12346)
+                .name("Studio Commercialista")
+                .email("mario.rossi@example.com")
+                .type(CompanyType.ACCOUNTANT)
+                .isAccountant(true)
+                .accountantId(12345)
+                .accessInfo(new CompanyInfoAccessInfo()
+                        .role(UserCompanyRole.MASTER)
+                        .throughAccountant(false)
+                )
+                .planInfo(new CompanyInfoPlanInfo()
+                        .limits(new CompanyInfoPlanInfoLimits()
+                                .clients(5000)
+                                .suppliers(5000)
+                                .products(5000)
+                                .documents(3000)
+                        )
+                        .functions(new CompanyInfoPlanInfoFunctions()
+                                .documentAttachments(true)
+                                .archive(true)
+                                .paymentNotifications(true)
+                                .paypal(true)
+                                .receipts(true)
+                                .eInvoice(true)
+                                .genius(true)
+                                .stock(true)
+                                .smtp(true)
+                                .mailTracking(true)
+                                .subaccounts(true)
+                                .tesseraSanitaria(true)
+                                .recurring(true)
+                                .sofort(false)
+                                .cerved(true)
+                                .tsDigital(true)
+                                .tsPay(true)
+                                .tsInvoiceTrading(true)
+                        )
+                        .functionsStatus(new CompanyInfoPlanInfoFunctionsStatus()
+                                .tsDigital(new FunctionStatus()
+                                        .active(true)
+                                )
+                                .tsPay(new FunctionStatus()
+                                        .active(false)
+                                )
+                        )
+                );
+
+        GetCompanyInfoResponse response = api.getCompanyInfo(companyId);
+        assertEquals(expected, response.getData());
+        Mockito.verify(mockCall, Mockito.only()).execute();
     }
-    
+
 }
