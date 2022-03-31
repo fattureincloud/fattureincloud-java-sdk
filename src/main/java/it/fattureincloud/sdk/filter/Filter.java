5e7b89ff71e1dd8433838146a5afcd5ade7c2dd7
Mauro Valota <7254293+valmoz@users.noreply.github.com>
2022-03-31 15:01:29 +0200

    fix: added filter helper (#35)

---
+++ src/main/java/it/fattureincloud/sdk/filter/Filter.java
@@ -0,0 +1,123 @@
+package it.fattureincloud.sdk.filter;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Filter {
+    private Expression expression;
+
+    public Filter() {
+        this.expression = null;
+    }
+
+    public Filter(Expression expression) {
+        this.expression = expression;
+    }
+
+    public Optional<Expression> getExpression() {
+        return Optional.ofNullable(expression);
+    }
+
+    public void setExpression(Expression expression) {
+        this.expression = expression;
+    }
+
+    public <T> Filter where(String field, Operator op, T value) {
+        this.expression = new Condition<>(field, op, value);
+        return this;
+    }
+
+    public Filter whereExpression(Expression expression) {
+        this.expression = expression;
+        return this;
+    }
+
+    public <T> Filter and(String field, Operator op, T value) throws Exception {
+        if (this.expression == null) {
+            throw new Exception("Cannot create a conjunction for an empty expression.");
+        }
+        Expression left = this.expression;
+        Expression right = new Condition<>(field, op, value);
+        this.expression = new Conjunction(left, right);
+        return this;
+    }
+
+    public Filter andExpression(Expression expression) throws Exception {
+        if (this.expression == null || expression == null) {
+            throw new Exception("Cannot create a conjunction for an empty expression.");
+        }
+        Expression left = this.expression;
+        this.expression = new Conjunction(left, expression);
+        return this;
+    }
+
+    public Filter andFilter(Filter filter) throws Exception {
+        if (this.expression == null || filter == null || !filter.getExpression().isPresent()) {
+            throw new Exception("Cannot create a conjunction for an empty expression.");
+        }
+        Expression left = this.expression;
+        this.expression = new Conjunction(left, filter.getExpression().get());
+        return this;
+    }
+
+    public <T> Filter or(String field, Operator op, T value) throws Exception {
+        if (this.expression == null) {
+            throw new Exception("Cannot create a disjunction for an empty expression.");
+        }
+        Expression left = this.expression;
+        Expression right = new Condition<>(field, op, value);
+        this.expression = new Disjunction(left, right);
+        return this;
+    }
+
+    public Filter orExpression(Expression expression) throws Exception {
+        if (this.expression == null || expression == null) {
+            throw new Exception("Cannot create a disjunction for an empty expression.");
+        }
+        Expression left = this.expression;
+        this.expression = new Disjunction(left, expression);
+        return this;
+    }
+
+    public Filter orFilter(Filter filter) throws Exception {
+        if (this.expression == null || filter == null || !filter.getExpression().isPresent()) {
+            throw new Exception("Cannot create a disjunction for an empty expression.");
+        }
+        Expression left = this.expression;
+        this.expression = new Disjunction(left, filter.getExpression().get());
+        return this;
+    }
+
+    public String buildQuery() {
+        if (this.expression == null) {
+            return "";
+        } else {
+            return this.expression.buildQuery();
+        }
+    }
+
+    public String buildUrlEncodedQuery() throws UnsupportedEncodingException {
+        return URLEncoder.encode(this.buildQuery(), StandardCharsets.UTF_8.toString());
+    }
+
+    @Override
+    public String toString() {
+        return this.buildQuery();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Filter filter = (Filter) o;
+        return Objects.equals(expression, filter.expression);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expression);
+    }
+}
---
+++ src/test/java/it/fattureincloud/sdk/filter/ValueTest.java
@@ -0,0 +1,62 @@
+package it.fattureincloud.sdk.filter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for Value
+ */
+public class ValueTest {
+    @BeforeEach
+    public void init() {
+    }
+
+    /**
+     * Tests for Value
+     */
+    @Test
+    public void testValue() {
+        Value<Boolean> valueBool = new Value<>(true);
+        assertEquals(true, valueBool.getValue());
+        assertEquals("true", valueBool.printValue());
+
+        valueBool.setValue(false);
+        assertEquals(false, valueBool.getValue());
+        assertEquals("false", valueBool.printValue());
+
+        Value<Integer> valueInt = new Value<>(22);
+        assertEquals(22, valueInt.getValue());
+        assertEquals("22", valueInt.printValue());
+
+        valueInt.setValue(88);
+        assertEquals(88, valueInt.getValue());
+        assertEquals("88", valueInt.printValue());
+
+        Value<Float> valueFloat = new Value<>(22.02f);
+        assertEquals(22.02f, valueFloat.getValue());
+        assertEquals("22.02", valueFloat.printValue());
+
+        valueFloat.setValue(19.88f);
+        assertEquals(19.88f, valueFloat.getValue());
+        assertEquals("19.88", valueFloat.printValue());
+
+        Value<Double> valueDouble = new Value<>(22.02);
+        assertEquals(22.02, valueDouble.getValue());
+        assertEquals("22.02", valueDouble.printValue());
+
+        valueDouble.setValue(19.88);
+        assertEquals(19.88, valueDouble.getValue());
+        assertEquals("19.88", valueDouble.printValue());
+
+        Value<String> valueStr = new Value<>("Paoletta");
+        assertEquals("Paoletta", valueStr.getValue());
+        assertEquals("'Paoletta'", valueStr.printValue());
+
+        valueStr.setValue("Dalmine");
+        assertEquals("Dalmine", valueStr.getValue());
+        assertEquals("'Dalmine'", valueStr.printValue());
+    }
+}
+
