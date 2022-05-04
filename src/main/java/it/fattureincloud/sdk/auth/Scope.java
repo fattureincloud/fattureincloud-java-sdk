6f4f5a82105c35cd4e010c40f32287b75cbec96c
Mauro Valota <maurovalota@fattureincloud.it>
2022-05-04 16:56:36 +0200

    chore: generated java

--- src/main/java/it/fattureincloud/sdk/auth/Scope.java
+++ src/main/java/it/fattureincloud/sdk/auth/Scope.java
@@ -1,41 +1,112 @@
 package it.fattureincloud.sdk.auth;
 
 public enum Scope {
-    SITUATION("situation:r"),
+
+    // Read permission to the Clients registry
     ENTITY_CLIENTS_READ("entity.clients:r"),
+
+    // Write permission to the Clients registry
     ENTITY_CLIENTS_ALL("entity.clients:a"),
+
+    // Read permission to the Suppliers registry
     ENTITY_SUPPLIERS_READ("entity.suppliers:r"),
+
+    // Write permission to the Suppliers registry
     ENTITY_SUPPLIERS_ALL("entity.suppliers:a"),
+
+    // Read permission to the Products
     PRODUCTS_READ("products:r"),
+
+    // Write permission to the Products
     PRODUCTS_ALL("products:a"),
+
+    // Read permission to the issued Invoices
     ISSUED_DOCUMENTS_INVOICES_READ("issued_documents.invoices:r"),
-    ISSUED_DOCUMENTS_CREDITNOTES_READ("issued_documents.credit_notes:r"),
+
+    // Read permission to the issued Credit Notes
+    ISSUED_DOCUMENTS_CREDIT_NOTES_READ("issued_documents.credit_notes:r"),
+
+    // Read permission to the issued Receipts
     ISSUED_DOCUMENTS_RECEIPTS_READ("issued_documents.receipts:r"),
+
+    // Read permission to the issued Orders
     ISSUED_DOCUMENTS_ORDERS_READ("issued_documents.orders:r"),
+
+    // Read permission to the issued Quotes
     ISSUED_DOCUMENTS_QUOTES_READ("issued_documents.quotes:r"),
+
+    // Read permission to the issued Proformas
     ISSUED_DOCUMENTS_PROFORMAS_READ("issued_documents.proformas:r"),
-    ISSUED_DOCUMENTS_DELIVERYNOTES_READ("issued_documents.delivery_notes:r"),
+
+    // Read permission to the issued Delivery Notes
+    ISSUED_DOCUMENTS_DELIVERY_NOTES_READ("issued_documents.delivery_notes:r"),
+
+    // Write permission to the issued Invoices
     ISSUED_DOCUMENTS_INVOICES_ALL("issued_documents.invoices:a"),
-    ISSUED_DOCUMENTS_CREDITNOTES_ALL("issued_documents.credit_notes:a"),
+
+    // Write permission to the issued Credit Notes
+    ISSUED_DOCUMENTS_CREDIT_NOTES_ALL("issued_documents.credit_notes:a"),
+
+    // Write permission to the issued issued Receipts
     ISSUED_DOCUMENTS_RECEIPTS_ALL("issued_documents.receipts:a"),
+
+    // Write permission to the issued Orders
     ISSUED_DOCUMENTS_ORDERS_ALL("issued_documents.orders:a"),
+
+    // Write permission to the issued Quotes
     ISSUED_DOCUMENTS_QUOTES_ALL("issued_documents.quotes:a"),
+
+    // Write permission to the issued Proformas
     ISSUED_DOCUMENTS_PROFORMAS_ALL("issued_documents.proformas:a"),
-    ISSUED_DOCUMENTS_DELIVERYNOTES_ALL("issued_documents.delivery_notes:a"),
+
+    // Write permission to the issued Delivery Notes
+    ISSUED_DOCUMENTS_DELIVERY_NOTES_ALL("issued_documents.delivery_notes:a"),
+
+    // Read permission to the issued Received Documents
     RECEIVED_DOCUMENTS_READ("received_documents:r"),
+
+    // Write permission to the issued Received Documents
     RECEIVED_DOCUMENTS_ALL("received_documents:a"),
+
+    // Read permission to the Stock movements
     STOCK_READ("stock:r"),
+
+    // Write permission to the Stock movements
     STOCK_ALL("stock:a"),
+
+    // Read permission to the Receipts
     RECEIPTS_READ("receipts:r"),
+
+    // Write permission to the Receipts
     RECEIPTS_ALL("receipts:a"),
+
+    // Read permission to the Taxes
     TAXES_READ("taxes:r"),
+
+    // Write permission to the Taxes
     TAXES_ALL("taxes:a"),
+
+    // Read permission to the Archive Documents
     ARCHIVE_READ("archive:r"),
+
+    // Read permission to the Archive Documents
     ARCHIVE_ALL("archive:a"),
+
+    // Read permission to the Cashbook
     CASHBOOK_READ("cashbook:r"),
+
+    // Write permission to the Cashbook
     CASHBOOK_ALL("cashbook:a"),
+
+    // Read permission to the Settings
     SETTINGS_READ("settings:r"),
-    SETTINGS_ALL("settings:a");
+
+    // Write permission to the Settings
+    SETTINGS_ALL("settings:a"),
+
+    // Read permission to the company Situation
+    SITUATION_READ("situation:r");
+
 
     private String scope;
 
