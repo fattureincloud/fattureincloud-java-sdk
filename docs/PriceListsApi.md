a9d54b961e31567e0adec942c8eeb025c323e9b4
Antonino Segreto <a.segreto@teamsystem.com>
2026-05-07 16:24:47 +0200

    change of description of pricelist id and added validate for recent LTS java sdk

--- docs/PriceListsApi.md
+++ docs/PriceListsApi.md
@@ -38,7 +38,7 @@ public class Example {
 
     PriceListsApi apiInstance = new PriceListsApi(defaultClient);
     Integer companyId = 12345; // Integer | The ID of the company.
-    String priceListId = "priceListId_example"; // String | The ID of the price
+    String priceListId = "priceListId_example"; // String | The pricelist ID
     try {
       GetPriceListItemsResponse result = apiInstance.getPriceListItems(companyId, priceListId);
       System.out.println(result);
@@ -58,7 +58,7 @@ public class Example {
 | Name | Type | Description  | Notes |
 |------------- | ------------- | ------------- | -------------|
 | **companyId** | **Integer**| The ID of the company. | |
-| **priceListId** | **String**| The ID of the price | |
+| **priceListId** | **String**| The pricelist ID | |
 
 ### Return type
 
--- src/main/java/it/fattureincloud/sdk/api/PriceListsApi.java
+++ src/main/java/it/fattureincloud/sdk/api/PriceListsApi.java
@@ -68,7 +68,7 @@ public class PriceListsApi {
    * Build call for getPriceListItems
    *
    * @param companyId The ID of the company. (required)
-   * @param priceListId The ID of the price (required)
+   * @param priceListId The pricelist ID (required)
    * @param _callback Callback for upload/download progress
    * @return Call to execute
    * @throws ApiException If fail to serialize the request body object
@@ -169,7 +169,7 @@ public class PriceListsApi {
    * Get PriceList Items List Retrieves all the Items of a PriceList
    *
    * @param companyId The ID of the company. (required)
-   * @param priceListId The ID of the price (required)
+   * @param priceListId The pricelist ID (required)
    * @return GetPriceListItemsResponse
    * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the
    *     response body
@@ -200,7 +200,7 @@ public class PriceListsApi {
    * Get PriceList Items List Retrieves all the Items of a PriceList
    *
    * @param companyId The ID of the company. (required)
-   * @param priceListId The ID of the price (required)
+   * @param priceListId The pricelist ID (required)
    * @return ApiResponse&lt;GetPriceListItemsResponse&gt;
    * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the
    *     response body
@@ -231,7 +231,7 @@ public class PriceListsApi {
    * Get PriceList Items List (asynchronously) Retrieves all the Items of a PriceList
    *
    * @param companyId The ID of the company. (required)
-   * @param priceListId The ID of the price (required)
+   * @param priceListId The pricelist ID (required)
    * @param _callback The callback to be executed when the API call finishes
    * @return The request call
    * @throws ApiException If fail to process the API call, e.g. serializing the request body object
